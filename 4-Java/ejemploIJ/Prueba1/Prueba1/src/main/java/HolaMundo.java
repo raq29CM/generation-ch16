83cd18746c2fa77cadde2f972a9f83f804e413b2
Raq2912CM <raquel.cruz2911@gmail.com>
2022-08-16 17:46:05 -0500

    Update del 16 de ago del 2022 - hasta actividades de JAVA

---
+++ 4-Java/ejemploIJ/Prueba1/Prueba1/src/main/java/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        // String miVariable = "Hola mundo Java";
+        String nombre = "Raquel";
+        System.out.println("saludos desde Java a: " + nombre);
+    }
+}
---
+++ 4-Java/ejemploJdk/Hola.java
@@ -0,0 +1,15 @@
+public class Hola{
+// esto es una clase del JDK Java
+
+	// String miVariable = "Hola mundo java ";
+
+	public static void main (String [] arg){
+	//String miVariable = "Hola mundo java saludo a:";
+	String miNombre = "Raquel Cruz";
+
+	// es la declaracion del metodo main
+	   System.out.println (" Hola saludos a: " + miNombre);
+	}
+
+}
+
