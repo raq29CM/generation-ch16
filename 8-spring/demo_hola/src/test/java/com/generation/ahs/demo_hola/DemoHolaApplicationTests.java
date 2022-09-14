cbe808e2ba36a2135bd40000912eb03c746c7263
Raq2912CM <raquel.cruz2911@gmail.com>
2022-09-14 16:32:55 -0500

    gradle update

---
+++ 8-spring/demo_hola/src/test/java/com/generation/ahs/demo_hola/DemoHolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.ahs.demo_hola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
